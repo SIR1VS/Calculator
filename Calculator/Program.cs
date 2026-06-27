086748e976905687a45ac3bb77415a5532f64cda
SIR1VS <mnasonov306@gmail.com>
2026-06-27 16:10:35 +0300

    Добавлена операция вычитания

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -9,6 +9,11 @@
             return number1 + number2;
         }
 
+        internal static double Subtract(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+
         static void Main(string[] args)
         {
         }
