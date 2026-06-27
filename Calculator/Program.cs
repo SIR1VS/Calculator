c28c4c8a3b5de4dd76b3dad1b578eacc29be453d
SIR1VS <mnasonov306@gmail.com>
2026-06-27 16:15:12 +0300

    Добавлена операция деления

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -19,6 +19,12 @@
             return number1 * number2;
         }
 
+        internal static double Divide(double number1, double number2)
+        {
+            if (number2 == 0) throw new DivideByZeroException("Деление на ноль недопустимо.");
+            return number1 / number2;
+        }
+
         static void Main(string[] args)
         {
         }
