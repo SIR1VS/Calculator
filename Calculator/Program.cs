38e4ff592be0d296bf3006094623bcf43e0d1c0f
SIR1VS <mnasonov306@gmail.com>
2026-06-27 16:06:04 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        internal delegate double Operation(double num1, double num2);
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
