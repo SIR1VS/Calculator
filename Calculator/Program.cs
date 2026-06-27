4ee3250ec268406965ced0030349fc5857f31619
SIR1VS <mnasonov306@gmail.com>
2026-06-27 16:08:55 +0300

    Добавлена операция сложения

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,6 +4,11 @@
     {
         internal delegate double Operation(double number1, double number2);
 
+        internal static double Add(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
         static void Main(string[] args)
         {
         }
