9d4a350f2c15ab1f6077f24e653c0baba84ede9d
SIR1VS <mnasonov306@gmail.com>
2026-06-27 16:25:47 +0300

    Добавлен консольный интерфейс калькулятора

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -27,6 +27,23 @@
 
         static void Main(string[] args)
         {
+            Console.WriteLine(new string('=', 38));
+            Console.WriteLine("\tКОНСОЛЬНЫЙ КАЛЬКУЛЯТОР\t");
+            Console.WriteLine(new string('=', 38));
+
+            Console.Write("Введите первое число: ");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Выберите операцию:");
+            Console.WriteLine("[ + ] - сложение");
+            Console.WriteLine("[ - ] - вычитание");
+            Console.WriteLine("[ * ] - умножение");
+            Console.WriteLine("[ / ] - деление");
+            Console.Write("Ваш выбор: ");
+            char choice = Console.ReadLine()[0];
         }
     }
 }
